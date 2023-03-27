9c635e3d5c341f30ebe753bbfb8f52d2ef630c6f
CaroFernando <0227782@up.edu.mx>
2023-03-27 00:35:58 -0600

    plc modules

--- PLCData/Form1.Designer.cs
+++ PLCData/Form1.Designer.cs
@@ -32,6 +32,9 @@
             this.State1 = new System.Windows.Forms.Button();
             this.UIPLCStatus1 = new PLCData.UIPLCStatus();
             this.UIPLCStatus2 = new PLCData.UIPLCStatus();
+            this.UIPLCStatus3 = new PLCData.UIPLCStatus();
+            this.UIPLCStatus4 = new PLCData.UIPLCStatus();
+            this.UIPLCStatus5 = new PLCData.UIPLCStatus();
             this.SuspendLayout();
             // 
             // State2
@@ -58,21 +61,45 @@
             // 
             this.UIPLCStatus1.Location = new System.Drawing.Point(54, 25);
             this.UIPLCStatus1.Name = "UIPLCStatus1";
-            this.UIPLCStatus1.Size = new System.Drawing.Size(281, 283);
+            this.UIPLCStatus1.Size = new System.Drawing.Size(207, 283);
             this.UIPLCStatus1.TabIndex = 5;
             // 
             // UIPLCStatus2
             // 
-            this.UIPLCStatus2.Location = new System.Drawing.Point(407, 25);
+            this.UIPLCStatus2.Location = new System.Drawing.Point(292, 25);
             this.UIPLCStatus2.Name = "UIPLCStatus2";
-            this.UIPLCStatus2.Size = new System.Drawing.Size(289, 286);
+            this.UIPLCStatus2.Size = new System.Drawing.Size(211, 286);
             this.UIPLCStatus2.TabIndex = 6;
+            // 
+            // UIPLCStatus3
+            // 
+            this.UIPLCStatus3.Location = new System.Drawing.Point(532, 25);
+            this.UIPLCStatus3.Name = "UIPLCStatus3";
+            this.UIPLCStatus3.Size = new System.Drawing.Size(192, 286);
+            this.UIPLCStatus3.TabIndex = 7;
+            // 
+            // UIPLCStatus4
+            // 
+            this.UIPLCStatus4.Location = new System.Drawing.Point(745, 25);
+            this.UIPLCStatus4.Name = "UIPLCStatus4";
+            this.UIPLCStatus4.Size = new System.Drawing.Size(186, 286);
+            this.UIPLCStatus4.TabIndex = 8;
+            // 
+            // UIPLCStatus5
+            // 
+            this.UIPLCStatus5.Location = new System.Drawing.Point(972, 25);
+            this.UIPLCStatus5.Name = "UIPLCStatus5";
+            this.UIPLCStatus5.Size = new System.Drawing.Size(180, 286);
+            this.UIPLCStatus5.TabIndex = 9;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(846, 544);
+            this.ClientSize = new System.Drawing.Size(1356, 552);
+            this.Controls.Add(this.UIPLCStatus5);
+            this.Controls.Add(this.UIPLCStatus4);
+            this.Controls.Add(this.UIPLCStatus3);
             this.Controls.Add(this.UIPLCStatus2);
             this.Controls.Add(this.UIPLCStatus1);
             this.Controls.Add(this.State1);
@@ -89,6 +116,9 @@
         private System.Windows.Forms.Button State1;
         private UIPLCStatus UIPLCStatus1;
         private UIPLCStatus UIPLCStatus2;
+        private UIPLCStatus UIPLCStatus3;
+        private UIPLCStatus UIPLCStatus4;
+        private UIPLCStatus UIPLCStatus5;
     }
 }
 
--- PLCData/Form1.cs
+++ PLCData/Form1.cs
@@ -16,26 +16,48 @@ namespace PLCData
     public partial class Form1 : Form
     {
 
-        TestRandomReader reader1;
-        MXComponentPLCReader reader2;
+        TestRandomReader testreader;
+        MXComponentPLCReader reader1, reader2, reader3, reader4;
         public Form1()
         {
             InitializeComponent();
-            this.reader1 = new TestRandomReader(2);
+            this.testreader = new TestRandomReader(1);
 
-            this.reader1.addConnectionCallback(new TestConnectionCallback());
-            this.reader1.addConnectionCallback(new APIConnectionCallback());
-            this.reader1.addDataCallback(new APICallDataCallback());
-            this.reader1.addDataCallback(new TestDataCallback());
-            this.UIPLCStatus1.setReader(this.reader1);
+            this.testreader.addConnectionCallback(new TestConnectionCallback());
+            this.testreader.addConnectionCallback(new APIConnectionCallback());
+            this.testreader.addDataCallback(new APICallDataCallback());
+            this.testreader.addDataCallback(new TestDataCallback());
+            this.UIPLCStatus1.setReader(this.testreader);
             this.UIPLCStatus1.setName("Test1");
 
-            this.reader2 = new MXComponentPLCReader(1, "D000", "D000", "D05", 0, 10);
+            this.reader1 = new MXComponentPLCReader(1, "D000", "D000", "D50", 0, 10);
+
+            this.reader1.addConnectionCallback(new TestConnectionCallback());
+            this.reader1.addDataCallback(new TestDataCallback());
+            this.UIPLCStatus2.setReader(this.reader1);
+            this.UIPLCStatus2.setName("Test2");
+
+            this.reader2 = new MXComponentPLCReader(2, "D001", "D001", "D51", 0, 10);
 
             this.reader2.addConnectionCallback(new TestConnectionCallback());
             this.reader2.addDataCallback(new TestDataCallback());
-            this.UIPLCStatus2.setReader(this.reader2);
-            this.UIPLCStatus2.setName("Test2");
+            this.UIPLCStatus3.setReader(this.reader2);
+            this.UIPLCStatus3.setName("Test3");
+
+            this.reader3 = new MXComponentPLCReader(3, "D002", "D002", "D52", 0, 10);
+
+            this.reader3.addConnectionCallback(new TestConnectionCallback());
+            this.reader3.addDataCallback(new TestDataCallback());
+            this.UIPLCStatus4.setReader(this.reader3);
+            this.UIPLCStatus4.setName("Test4");
+
+            this.reader4 = new MXComponentPLCReader(4, "D003", "D003", "D53", 0, 10);
+
+            this.reader4.addConnectionCallback(new TestConnectionCallback());
+            this.reader4.addDataCallback(new TestDataCallback());
+            this.UIPLCStatus5.setReader(this.reader4);
+            this.UIPLCStatus5.setName("Test5");
+
 
         }
 
@@ -46,7 +68,7 @@ namespace PLCData
 
         private void button1_Click(object sender, EventArgs e)
         {
-            this.reader1.ok = !this.reader1.ok;
+            this.testreader.ok = !this.testreader.ok;
         }
 
         private void uiplcStatus1_Load(object sender, EventArgs e)
